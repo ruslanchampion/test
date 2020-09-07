1a553e1fef5371eba732859eb19bc82fcaa40206
Ruslan <67142945+ruslanchampion@users.noreply.github.com>
2020-09-07 13:26:17 +0800

    Udt: look

--- prostSostav.js
+++ prostSostav.js
@@ -1,10 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
 
-function newFunction(number) {
-    let n = number;
-    nextPrime: for (let i = 2; i <= n; i++) {
-        for (let j = 2; j < i; j++) {
-            if (i % j == 0) { console.log(`${i} составное`); continue nextPrime; }
-        } console.log(`${i} простое`);
-    }
-}
-newFunction(100);
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OddEven</title>
+</head>
+
+<body>
+    <input class="input" type="number">
+    <button id="gen">generation</button>
+    <input class="output" type="text">
+
+    <script>
+
+        function newFunction(huy) {
+            huy = document.querySelector('.input').value;
+            let o = document.querySelector('.output');
+            nextPrime: for (let i = 2; i <= huy; i++) {
+                for (let j = 2; j < i; j++) {
+                    if (i % j == 0) {
+                        o.value = `${i} составное`;
+                        continue nextPrime;
+                    }
+                } o.value = (`${i} простое`);
+            }
+        
+
+        }
+
+        gen.addEventListener("click", newFunction);
+
+    </script>
+</body>
+
+</html>
